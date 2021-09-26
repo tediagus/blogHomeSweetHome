87ab21761aac27a8d6c3562b35ea8013e8861c6b
t3di <tediaga.c@gmail.com>
2021-09-26 23:13:53 +0200

    Update formataage fichier CRLF to LF

--- src/services/event.service.js
+++ src/services/event.service.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 /* process.env !== 'production' ? 'http://localhost:3000' : */
-export const EVENT_URL = 'https://homeapi-tediagus.vercel.app';
+export const EVENT_URL = 'https://homeapi.vercel.app/';
+// export const EVENT_URL = 'http://localhost:3000';
 
 const EventService = {
   findAll: async () => {
